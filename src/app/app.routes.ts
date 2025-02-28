f81d021ab1fc6debfae9f7f7bf8abe981fc423fb
Arun <arunk26544@gmail.com>
2025-02-28 12:27:06 +0530

    hii

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
 export const routes: Routes = [
     {
         path: "", 
         component: HomeComponent
+    },
+    {
+        path: "aboutus", 
+        component: AboutusComponent
     },
 ];
